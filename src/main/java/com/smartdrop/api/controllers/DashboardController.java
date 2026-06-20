997ac1b6c6bd1a7d4dee3cf9e420aee0e4c976e9
Rafael Barrenechea <u202417417@upc.edu.pe>
2026-06-20 04:31:32 -0500

    Agregado Dockerfile para Render

--- src/main/java/com/smartdrop/api/controllers/DashboardController.java
+++ src/main/java/com/smartdrop/api/controllers/DashboardController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/dashboard")
 public class DashboardController {
 
